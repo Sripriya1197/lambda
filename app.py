6de28767ebbd83fafbac382a1cf5e57b32557611
Sripriya1197 <rsripriyadharshini@gmail.com>
2025-05-06 19:22:34 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,9 @@
+def handler(event, context):
+    temp_f = 77
+    temp_c = (temp_f - 32) * 5 / 9
+    message = f"{temp_f}°F is equal to {temp_c:.2f}°C"
+
+    return {
+        "statusCode": 200,
+        "body": message
+    }
